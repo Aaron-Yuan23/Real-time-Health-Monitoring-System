f08ba64f582bb53c8ed01444b4e489af5541de73
Aaron Yuan <aaronyuan27@gmail.com>
2019-05-12 21:00:05 -0400

    Add files via upload

---
+++ radarcode/runSystem.bat
@@ -0,0 +1,8 @@
+
+SET PATH="/opt/anaconda3/bin:$PATH"
+SET PYTHONPATH=C:\Users\JiaQi\AppData\Local\Programs\Python\Python36\
+
+cd nuc/
+echo "establishing radar connection"
+
+C:\Users\JiaQi\AppData\Local\Programs\Python\Python36\python.exe main.py
